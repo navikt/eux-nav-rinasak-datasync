5aa9374ef525480400813daeb37972da58097b88
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2023-09-18 13:17:12 +0200

    case store client

--- eux-nav-rinasak-datasync-integration/src/main/kotlin/eux/nav/rinasak/datasync/integration/casestore/EuxCaseStoreClient.kt
+++ eux-nav-rinasak-datasync-integration/src/main/kotlin/eux/nav/rinasak/datasync/integration/casestore/EuxCaseStoreClient.kt
@@ -1,8 +1,24 @@
 package eux.nav.rinasak.datasync.integration.casestore
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Component
+import org.springframework.web.client.RestTemplate
+
 
 @Component
-class EuxCaseStoreClient {
+class EuxCaseStoreClient(
+    @Value("endpoint.eux.casestore")
+    val euxCaseStoreUrl: String,
+    val euxCaseStoreRestTemplate: RestTemplate
+) {
 
+    fun cases() =
+        euxCaseStoreRestTemplate
+            .exchange(
+                "${euxCaseStoreUrl}/cases",
+                HttpMethod.GET, null, object : ParameterizedTypeReference<List<EuxCaseStoreCase>>() {}
+            )
+            .body
 }

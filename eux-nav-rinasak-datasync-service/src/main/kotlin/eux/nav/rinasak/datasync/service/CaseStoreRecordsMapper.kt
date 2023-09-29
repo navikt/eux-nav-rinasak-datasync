d33f953dcb5238a83347d3e668e41025d3da58c1
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2023-09-29 10:42:48 +0200

    clean

--- eux-nav-rinasak-datasync-service/src/main/kotlin/eux/nav/rinasak/datasync/service/CaseStoreRecordsMapper.kt
+++ eux-nav-rinasak-datasync-service/src/main/kotlin/eux/nav/rinasak/datasync/service/CaseStoreRecordsMapper.kt
@@ -1,11 +1,2 @@
 package eux.nav.rinasak.datasync.service
 
-import eux.nav.rinasak.datasync.integration.casestore.EuxCaseStoreCase
-import eux.nav.rinasak.datasync.integration.casestore.EuxCaseStoreClient
-import eux.nav.rinasak.datasync.model.CaseStoreRecord
-import eux.nav.rinasak.datasync.model.SyncStatus.PENDING
-import eux.nav.rinasak.datasync.persistence.CaseStoreRecordRepository
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import org.springframework.stereotype.Service
-import java.util.*

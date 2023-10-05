38b0984cf0ee8a786ed454124fe64a7e08245943
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2023-10-05 12:02:26 +0200

    fagsakId ?

--- eux-nav-rinasak-datasync-integration/src/main/kotlin/eux/nav/rinasak/datasync/integration/saf/SafSak.kt
+++ eux-nav-rinasak-datasync-integration/src/main/kotlin/eux/nav/rinasak/datasync/integration/saf/SafSak.kt
@@ -1,7 +1,7 @@
 package eux.nav.rinasak.datasync.integration.saf
 
 data class SafSak(
-    val fagsakId: String,
+    val fagsakId: String?,
     val sakstype: String? = null,
     val tema: String? = null,
     val fagsaksystem: String? = null,

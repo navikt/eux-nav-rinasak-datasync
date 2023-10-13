0c78c5e100d8fb431d52e841153e3fd4ecead1ed
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2023-10-13 14:34:34 +0200

    sedId

--- eux-nav-rinasak-datasync-service/src/main/kotlin/eux/nav/rinasak/datasync/service/navrinasak/SendToNavRinasakFacadeService.kt
+++ eux-nav-rinasak-datasync-service/src/main/kotlin/eux/nav/rinasak/datasync/service/navrinasak/SendToNavRinasakFacadeService.kt
@@ -1,5 +1,6 @@
 package eux.nav.rinasak.datasync.service.navrinasak
 
+import eux.nav.rinasak.datasync.model.Dokument
 import eux.nav.rinasak.datasync.model.SyncStatus.PENDING
 import eux.nav.rinasak.datasync.persistence.DokumentRepository
 import eux.nav.rinasak.datasync.persistence.InitiellFagsakRepository
@@ -8,6 +9,8 @@ import eux.nav.rinasak.datasync.service.casestore.CaseStoreRecordsService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import java.util.*
+import java.util.UUID.fromString
 
 @Service
 class SendToNavRinasakFacadeService(
@@ -25,11 +28,30 @@ class SendToNavRinasakFacadeService(
             .associateBy { it.navRinasakUuid }
         val dokumenterPending = dokumentRepository
             .findAllBySyncStatus(PENDING)
+            .filter { it.gyldigSedId() }
+            .map { it.copy(sedId = uuidString(it.sedId)) }
             .groupBy { it.navRinasakUuid }
         log.info("Sender ${navRinasakerPending.count()} NAV Rinasaker...")
         navRinasakerPending
             .forEach { integrationService.sync(it, initielleFagsakerPending, dokumenterPending) }
         log.info("Ferdig med oversending av ${navRinasakerPending.count()} NAV Rinasaker!")
         return navRinasakerPending.count()
+    }
+
+    fun Dokument.gyldigSedId() =
+        try {
+            uuid(uuidWithoutDash = sedId)
+            true
+        } catch (e: RuntimeException) {
+            log.error("ugyldig sedId: $sedId", e)
+            false
+        }
+
+    fun uuid(uuidWithoutDash: String): UUID =
+        fromString(uuidString(uuidWithoutDash = uuidWithoutDash))
+
+    fun uuidString(uuidWithoutDash: String): String = with(uuidWithoutDash) {
+        listOf(substring(0, 8), substring(8, 12), substring(12, 16), substring(16, 20), substring(20, 32))
+            .joinToString(separator = "-")
     }
 }

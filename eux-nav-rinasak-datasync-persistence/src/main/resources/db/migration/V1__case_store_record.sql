4bea9a5b84b6e4fb303f530f03b8d59fd5e3e7c9
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2023-10-16 13:34:16 +0200

    not null

--- eux-nav-rinasak-datasync-persistence/src/main/resources/db/migration/V1__case_store_record.sql
+++ eux-nav-rinasak-datasync-persistence/src/main/resources/db/migration/V1__case_store_record.sql
@@ -1,7 +1,7 @@
 CREATE TABLE case_store_record
 (
     case_store_record_uuid uuid primary key,
-    rinasak_id             integer,
+    rinasak_id             integer not null,
     fagsak_id              varchar(100),
     case_store_id          varchar(100),
     fagsak_tema            varchar(100),
--- eux-nav-rinasak-datasync-persistence/src/main/resources/db/migration/V2__nav_rinasak.sql
+++ eux-nav-rinasak-datasync-persistence/src/main/resources/db/migration/V2__nav_rinasak.sql
@@ -1,7 +1,7 @@
 CREATE TABLE nav_rinasak
 (
     nav_rinasak_uuid       uuid primary key,
-    rinasak_id             integer,
+    rinasak_id             integer not null,
     overstyrt_enhetsnummer varchar(31),
     sync_status            varchar(32),
     opprettet_bruker       varchar(100),

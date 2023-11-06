ae15a407c6583a79bfb1b6279591e8a2ffe655e3
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2023-11-06 16:05:37 +0100

    .

--- eux-nav-rinasak-datasync-persistence/src/main/resources/db/migration/V1__case_store_record.sql
+++ eux-nav-rinasak-datasync-persistence/src/main/resources/db/migration/V1__case_store_record.sql
@@ -7,7 +7,7 @@ CREATE TABLE case_store_record
     fagsak_tema            varchar(100),
     overstyrt_enhetsnummer varchar(32),
     journalpost_id         varchar(32),
-    sync_status            varchar(32),
+    sync_status            varchar(36),
     buc_id                 uuid,
     opprettet_bruker       varchar(100),
     opprettet_dato         timestamp

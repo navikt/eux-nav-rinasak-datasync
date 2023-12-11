d90244ad04ad02c27cbad2119500c4e3de54f17a
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2023-12-11 13:31:14 +0100

    diff for reset

--- eux-nav-rinasak-datasync-persistence/src/main/resources/db/migration/V2__nav_rinasak.sql
+++ eux-nav-rinasak-datasync-persistence/src/main/resources/db/migration/V2__nav_rinasak.sql
@@ -2,7 +2,7 @@ CREATE TABLE nav_rinasak
 (
     nav_rinasak_uuid       uuid primary key,
     rinasak_id             integer not null,
-    overstyrt_enhetsnummer varchar(31),
+    overstyrt_enhetsnummer varchar(32),
     sync_status            varchar(32),
     opprettet_bruker       varchar(100),
     opprettet_dato         timestamp

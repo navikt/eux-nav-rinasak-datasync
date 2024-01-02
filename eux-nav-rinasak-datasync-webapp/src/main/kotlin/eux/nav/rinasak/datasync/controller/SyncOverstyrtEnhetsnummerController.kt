ce79334f0918e501f27dbda8c0be523c5372308f
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2024-01-02 16:10:11 +0100

    overstyrt enhetsnummer

---
+++ eux-nav-rinasak-datasync-webapp/src/main/kotlin/eux/nav/rinasak/datasync/controller/SyncOverstyrtEnhetsnummerController.kt
@@ -0,0 +1,33 @@
+package eux.nav.rinasak.datasync.controller
+
+import eux.nav.rinasak.datasync.service.enhet.OverstyrtEnhetsnummerService
+import no.nav.security.token.support.core.api.Unprotected
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+
+@Unprotected
+@Controller
+class SyncOverstyrtEnhetsnummerController(
+    val overstyrtEnhetsnummerService: OverstyrtEnhetsnummerService,
+) {
+
+    @GetMapping("/populate-all-case-store-records")
+    fun populateAllCaseStoreRecords(
+        model: Model,
+    ): String {
+        model.addAttribute("number", 0)
+        return "sync-overstyrt-enhetsnummer"
+    }
+
+    @PostMapping("/populate-all-case-store-records")
+    fun populateALlCaseStoreRecords(
+        @RequestParam("inputNumber") inputNumber: Int?,
+        model: Model
+    ): String {
+        overstyrtEnhetsnummerService.sync()
+        return "sync-overstyrt-enhetsnummer"
+    }
+}

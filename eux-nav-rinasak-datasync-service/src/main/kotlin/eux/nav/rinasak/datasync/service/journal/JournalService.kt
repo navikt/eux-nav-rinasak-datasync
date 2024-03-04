276a7cae352eb15736b83cf373ddd8081ad0a47a
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2024-03-04 15:35:07 +0100

    hrmf

--- eux-nav-rinasak-datasync-service/src/main/kotlin/eux/nav/rinasak/datasync/service/journal/JournalService.kt
+++ eux-nav-rinasak-datasync-service/src/main/kotlin/eux/nav/rinasak/datasync/service/journal/JournalService.kt
@@ -1,4 +1,21 @@
 package eux.nav.rinasak.datasync.service.journal
 
-class JournalService {
+import eux.nav.rinasak.datasync.integration.saf.SafClient
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.stereotype.Service
+
+@Service
+class JournalService(
+    val safClient: SafClient
+) {
+
+    val log = KotlinLogging.logger {}
+
+    fun journal(journalposter: List<String>) {
+        journalposter
+            .forEach {
+                val journalpost = safClient.safJournalpost(it)
+                log.info { "journalpost: $journalpost" }
+            }
+    }
 }
